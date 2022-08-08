2facb31bfe6d6d041059e5c1e90a4ba6611c9d08
msd <87383399+msd@users.noreply.github.com>
2022-08-08 06:38:29 +0100

    __declspec only appears for MSVC compiler

--- hello.hpp
+++ hello.hpp
@@ -1,6 +1,17 @@
 #pragma once
 
-#include <string>
+// attempt to detect MSVC in case cmake did not.
+// intel compiler defines _MSC_VER and also __INTEL_COMPILER.
+#if !defined(MSVC_COMPILER) && _MSC_VER && !__INTEL_COMPILER 
+#  define MSVC_COMPILER 1
+#endif
 
-extern "C" __declspec( dllexport ) // this line only needed for msvc, it does nothing for gcc
-const char * regular_greeting();
+#if MSVC_COMPILER
+	// this line only needed for msvc, it does nothing for gcc
+#  define DECLSPEC extern "C" __declspec( dllexport )
+#else
+#  define DECLSPEC
+#endif
+
+DECLSPEC const char * regular_greeting();
+
